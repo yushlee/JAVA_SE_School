159be7b0615b6565dafcf25e87fb37dcff96349b
yushlee <ktbwn@hotmail.com>
2020-08-17 20:29:21 +0800

    0817

---
+++ 0817/PrimitiveDataType.java
@@ -0,0 +1,32 @@
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		byte byteNumber = 1;
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		// 短整數
+		short shortNumber = 1;
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);		
+		// 整數
+		int intNumber = -125;
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		// 長整數
+		long longNumber = 123;
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		
+		// 小數點的部份
+		float floatNumber = 3.12f;
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		
+		double doubleNumber = 3.12d;
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);		
+		
+	}
+
+}
