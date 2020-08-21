fdfe874063c5534f9e18c9a42b99789d75a2ad5e
yushlee <ktbwn@hotmail.com>
2020-08-21 21:46:45 +0800

    LoopSyntax.java

--- 0821/LoopSyntax.java
+++ 0821/LoopSyntax.java
@@ -4,10 +4,12 @@ public class LoopSyntax {
 
 	public static void main(String[] args) {
 		
-//		for(int i = 0 ; i < 10 ;  i++ ) {
-			// 重覆執行的事情
-//			System.out.println(i);
-//		}
+
+		
+		for(int i = 1 ; i < 10 ;  i = i + 2 ) {
+//			 重覆執行的事情
+			System.out.println(i);
+		}
 		
 		// 無窮迴圈
 		for(int i = 0 ; i < 10 ; ) {
@@ -15,11 +17,26 @@ public class LoopSyntax {
 			i++;
 		}
 		
-		for(int i = 0 ;  ; i++) {
-			System.out.println("Hello!");
-			
-		}		
-
+//		for(int i = 0 ;  ; i++) {
+//			System.out.println("Hello!");
+//		}		
+		
+		// 初始變數
+		int j = 0;
+		// 條件式(前測試)
+		while(j <= 5){
+			System.out.println("Hello:" + j);
+			// j++; // 遞增式
+//			j = j + 3;
+			j += 3;
+		}
+		
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i != 0);
+		
 	}
 
 }
