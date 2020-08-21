35a60d477cf708a724ff66ac68e91eb1c485bbf9
yushlee <ktbwn@hotmail.com>
2020-08-21 21:22:53 +0800

    LoopSyntax

---
+++ 0821/LoopSyntax.java
@@ -0,0 +1,25 @@
+package com.training.session3;
+
+public class LoopSyntax {
+
+	public static void main(String[] args) {
+		
+//		for(int i = 0 ; i < 10 ;  i++ ) {
+			// 重覆執行的事情
+//			System.out.println(i);
+//		}
+		
+		// 無窮迴圈
+		for(int i = 0 ; i < 10 ; ) {
+			System.out.println("Hello!");
+			i++;
+		}
+		
+		for(int i = 0 ;  ; i++) {
+			System.out.println("Hello!");
+			
+		}		
+
+	}
+
+}
