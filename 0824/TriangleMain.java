f409f04ea23949512412d0e87afe8e50f2a66643
yushlee <ktbwn@hotmail.com>
2020-08-24 19:34:31 +0800

    0824

---
+++ 0824/TriangleMain.java
@@ -0,0 +1,10 @@
+package com.training.session3;
+
+public class TriangleMain {
+	public static void main(String[] args) {
+		
+		PrintStar printStar = new PrintStar();
+		printStar.printTriangle(5);
+//		printStar.printStar(6);
+	}
+}
