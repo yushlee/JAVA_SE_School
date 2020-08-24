6339b27deb1350d278dedeb241503a3f2eb081a3
yushlee <ktbwn@hotmail.com>
2020-08-24 20:36:39 +0800

    OneArrayExp

--- 0824/TriangleMain.java
+++ 0824/TriangleMain.java
@@ -3,8 +3,11 @@ package com.training.session3;
 public class TriangleMain {
 	public static void main(String[] args) {
 		
-		PrintStar printStar = new PrintStar();
-		printStar.printTriangle(5);
-//		printStar.printStar(6);
+//		PrintStar printStar = new PrintStar();
+//		printStar.printTriangle(5);
+
+		
+		Triangle triangle = new Triangle();
+		triangle.printTriangle(5);
 	}
 }
