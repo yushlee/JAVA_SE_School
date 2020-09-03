2f049b4136043eabd0400367a0776fe397750998
yushlee <ktbwn@hotmail.com>
2020-09-03 19:45:37 +0800

    Test/ArithmeticString.java

---
+++ Test/ArithmeticString.java
@@ -0,0 +1,14 @@
+
+
+public class ArithmeticString {
+
+	public static void main(String[] args) {
+		// 給定一個字串如: "1+3-5-2+5+8"
+		// 算式裡只會有加跟減
+		// 執行結果
+		// "1+3-5-2+5+8" = 10; 
+		String inputText = "1+3-5-2+5-12";
+	
+	}
+
+}
