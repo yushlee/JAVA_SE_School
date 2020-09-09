b310b30184611f75123d8571ab3df1fe837ec9c7
yushlee <ktbwn@hotmail.com>
2020-09-09 20:45:04 +0800

    0909/session6/ClassTypeExp.java

--- 0909/session6/ClassTypeExp.java
+++ 0909/session6/ClassTypeExp.java
@@ -3,6 +3,7 @@ package com.training.session6;
 public class ClassTypeExp {
 
 	public static void main(String[] args) {
+		
 		// 具體類別
 		Concrete concrete = new Concrete();
 		concrete.concreteMethod();
@@ -12,6 +13,18 @@ public class ClassTypeExp {
 		// Abstract abstractClass = new Abstract();
 		Abstract abstractClass = new SubClass();
 		abstractClass.abstractMethod();
+		
+		// 介面
+		// 不能直接實體化須透過其子類別來實作
+//		Interface interfaceClass = new Interface();
+		Interface interfaceClass = new SubClass2();
+		interfaceClass.interfaceMethod1();		
+		System.out.println(Interface.text);
+		
+		// The final field Interface.text cannot be assigned
+//		Interface.text = "B";
+		
+		
 	}
 
 }
@@ -30,6 +43,7 @@ class Concrete {
 
 // Abstract Class (抽象類別)
 abstract class Abstract {
+	
 	// 半抽象半具體：抽象方法與具體方法(有實作內容的方法)並存
 	public void concreteMethod() {
 		// 具體類別方法的實作內容
@@ -38,6 +52,8 @@ abstract class Abstract {
 	 
 	// 抽象方法
 	public abstract void abstractMethod();
+	
+	public abstract void abstractMethod2();
 }
 
 class SubClass extends Abstract {
@@ -47,5 +63,45 @@ class SubClass extends Abstract {
 		System.out.println("abstractMethod");
 	}
 	
+	@Override
+	public void abstractMethod2() {
+		System.out.println("abstractMethod2");
+	}	
+}
+
+// 介面
+// 絕對抽象：不允許具體方法
+interface Interface {
+	
+	// 常數(static 靜態、final 唯一、public 公開)
+	String text = "A";
+	
+	// 介面方法
+	public void interfaceMethod1();
+	
+	// 介面方法 abstract 修飾可省略
+	public abstract void interfaceMethod2();
+	
+//	public void concreteMethod() {
+		// 具體類別方法的實作內容
+//		System.out.println("concreteMethod");
+//	}
+}
+
+
+class SubClass2 implements Interface {
+
+	@Override
+	public void interfaceMethod1() {
+		System.out.println("interfaceMethod1 !!!");
+	}
+
+	@Override
+	public void interfaceMethod2() {
+		
+	}
 	
 }
+
+
+
