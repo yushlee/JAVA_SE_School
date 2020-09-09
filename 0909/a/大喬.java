5267d315f7bb55fea72bf2b4cd271e539cea9141
yushlee <ktbwn@hotmail.com>
2020-09-09 22:21:10 +0800

    0909/a/大喬.java

---
+++ 0909/a/大喬.java
@@ -0,0 +1,21 @@
+package com.training.session6.a;
+
+public class 大喬 extends 女孩{
+	
+	@Override
+	public void 認識(){
+		System.out.println("認識:薰衣草森林");
+	}
+	
+	@Override
+	public void 交往(){
+		System.out.println("交往:米其林三星餐廳");
+	}
+	
+	@Override
+	public void 結婚(){
+		System.out.println("結婚:杜拜海灘");
+	}
+	
+
+}
