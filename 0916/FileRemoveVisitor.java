0dec61669667973ea3f4dc67153ac70c672083b7
yushlee <ktbwn@hotmail.com>
2020-09-16 21:08:04 +0800

    0916/FileCopyVisitor.java

---
+++ 0916/FileRemoveVisitor.java
@@ -0,0 +1,24 @@
+package com.training.session6.nio;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileRemoveVisitor extends SimpleFileVisitor<Path> {
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		Files.delete(file);
+		return FileVisitResult.CONTINUE;
+	}
+	
+	@Override
+	public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+		Files.delete(dir);
+		return FileVisitResult.CONTINUE;
+	}
+
+}
--- 0916/WalkFileTreeExample.java
+++ 0916/WalkFileTreeExample.java
@@ -1,19 +1,27 @@
 package com.training.session6.nio;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class WalkFileTreeExample {
 
 	public static void main(String[] args) {
+//		Path path = Paths.get("D:/").toAbsolutePath();
 		Path path = Paths.get("dir/session6").toAbsolutePath();
+		
 		try {
-			Files.walkFileTree(path, new PrintTree());
+//			Files.walkFileTree(path, new PrintTree());
+			
+			Files.walkFileTree(path, new SimplePrintTree());
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -41,6 +49,15 @@ class PrintTree implements FileVisitor<Path> {
 			throws IOException {
 		
 		System.out.println("讀取「檔案時」要做的事:" + file);
+
+//		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.java");
+		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.{mp4,mov,avi,flv,wmv,mkv}");
+		Path fileName = file.getFileName();
+		if(matcher.matches(fileName)){
+			System.out.println(fileName + " 吼! 抓到了吼!");
+		}else{
+			System.out.println(fileName + " not Java file");
+		}
 		
 		return FileVisitResult.CONTINUE;
 	}
@@ -60,8 +77,25 @@ class PrintTree implements FileVisitor<Path> {
 			throws IOException {
 
 		return FileVisitResult.CONTINUE;
+	}	
+}
+
+
+class  SimplePrintTree  extends  SimpleFileVisitor<Path> {
+	
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+			throws IOException {
+
+
+		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.java");
+//		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.{mp4,mov,avi,flv,wmv,mkv}");
+		Path fileName = file.getFileName();
+		if(matcher.matches(fileName)){
+			System.out.println(file);
+		}
+		
+		return FileVisitResult.CONTINUE;
 	}
-
-
 	
 }
