3950651303e9d5aa42c159ab315d6e05078c05a9
yushlee <ktbwn@hotmail.com>
2020-10-17 16:10:16 +0800

    Calculator.java

---
+++ 1017/Calculator.java
@@ -0,0 +1,25 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		int a = 7;
+		
+		int b = 3;
+		
+		System.out.println(a + b - 12);
+		
+		System.out.println(a * b);
+		
+		System.out.println(a / b); // 取整數「商數」
+		
+		System.out.println(a % b); // 取餘數
+		
+		System.out.println(a + b * 12);
+		
+		System.out.println(   (a + b) * 12 );
+		
+		System.out.println(a + b / 12);
+	}
+
+}
